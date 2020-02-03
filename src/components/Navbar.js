e9d4fb18d224380cfdc39c92b96a14f0d366d016
hkr hasan <hkrhasan8010@gmail.com>
2020-02-03 04:01:27 -0800

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className='navbar navbar-expand-sm navbar-dark px-sm-5'>
+
+                {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */}
+            
+                <Link to='/'>
+                    <img src={} alt='' />
+                </Link>
+            
+            </nav>
+        )
+    }
+}
